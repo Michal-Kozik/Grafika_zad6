5cccf57f69f9e5aca0f384f9853d89f7a50ecef6
Michal Kozik <michal.kozikowski99@gmail.com>
2021-11-15 15:08:32 +0100

    Add project files.

---
+++ Grafika_zad6/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿using System.Windows.Forms;
+
+namespace Grafika_zad6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxBezier = new System.Windows.Forms.PictureBox();
+            this.numericUpDownPoint = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownX = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownY = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBezier)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPoint)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxBezier
+            // 
+            this.pictureBoxBezier.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxBezier.Name = "pictureBoxBezier";
+            this.pictureBoxBezier.Size = new System.Drawing.Size(426, 426);
+            this.pictureBoxBezier.TabIndex = 0;
+            this.pictureBoxBezier.TabStop = false;
+            this.pictureBoxBezier.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox_MouseClick);
+            // 
+            // numericUpDownPoint
+            // 
+            this.numericUpDownPoint.Location = new System.Drawing.Point(500, 93);
+            this.numericUpDownPoint.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownPoint.Name = "numericUpDownPoint";
+            this.numericUpDownPoint.Size = new System.Drawing.Size(288, 20);
+            this.numericUpDownPoint.TabIndex = 1;
+            this.numericUpDownPoint.ValueChanged += new System.EventHandler(this.NumericUpDownPoint_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(444, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Punkt nr:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(605, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Współrzędne:";
+            // 
+            // numericUpDownX
+            // 
+            this.numericUpDownX.Location = new System.Drawing.Point(500, 203);
+            this.numericUpDownX.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownX.Name = "numericUpDownX";
+            this.numericUpDownX.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownX.TabIndex = 4;
+            // 
+            // numericUpDownY
+            // 
+            this.numericUpDownY.Location = new System.Drawing.Point(667, 203);
+            this.numericUpDownY.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownY.Name = "numericUpDownY";
+            this.numericUpDownY.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownY.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(477, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "X:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(644, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Y:";
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(604, 238);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 8;
+            this.buttonUpdate.Text = "Aktualizuj";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.ButtonUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDownY);
+            this.Controls.Add(this.numericUpDownX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownPoint);
+            this.Controls.Add(this.pictureBoxBezier);
+            this.Name = "Form1";
+            this.Text = "Grafika zadanie nr 6";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBezier)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPoint)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBoxBezier;
+        private NumericUpDown numericUpDownPoint;
+        private Label label1;
+        private Label label2;
+        private NumericUpDown numericUpDownX;
+        private NumericUpDown numericUpDownY;
+        private Label label3;
+        private Label label4;
+        private Button buttonUpdate;
+    }
+}
+
---
+++ Grafika_zad6/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grafika_zad6
+{
+    public partial class Form1 : Form
+    {
+        private List<double> pointList = new List<double>();
+
+        SolidBrush px = new SolidBrush(Color.Black);
+        Pen newpx = new Pen(Brushes.Red);
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            pointList.Add(e.X);
+            pointList.Add(e.Y);
+            numericUpDownPoint.Value++;
+            // rysowanie;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = Graphics.FromHwnd(pictureBoxBezier.Handle);
+        }
+
+        private void ButtonUpdate_Click(object sender, EventArgs e)
+        {
+            if (numericUpDownPoint.Value <= pointList.Count / 2 && numericUpDownPoint.Value != 0)
+            {
+                pointList[(int)numericUpDownPoint.Value * 2 - 2] = Int32.Parse(numericUpDownX.Value.ToString());
+                pointList[(int)numericUpDownPoint.Value * 2 - 1] = Int32.Parse(numericUpDownY.Value.ToString());
+                // rysowanie;
+            }
+        }
+
+        private void NumericUpDownPoint_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDownPoint.Value <= pointList.Count / 2 && numericUpDownPoint.Value != 0)
+            {
+                numericUpDownX.Value = (decimal)pointList[(int)numericUpDownPoint.Value * 2 - 2];
+                numericUpDownY.Value = (decimal)pointList[(int)numericUpDownPoint.Value * 2 - 1];
+            }
+            else
+            {
+                numericUpDownX.Value = 0;
+                numericUpDownX.Value = 0;
+            }
+        }
+
+    }
+}
